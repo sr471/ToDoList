6d6e11c57f47931550f545da6be0bb240e6e8cb2
sr471 <shanthi.robinson100@gmail.com>
2016-04-19 09:13:42 -0700

    Basic Milestone 1

---
+++ public_html/js/ToDoList.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function (){
+    var APPLICATION_ID = "CFDECE62-F954-CB53-FFFC-8AE2A5150500",
+        SECRET_KEY ="3D5326F5-315C-E478-FFFD-0762B5266E00",
+        VERSION = "v1";
+        
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+});
+
